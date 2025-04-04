6472cc224ef4fde730721ea3f3c2e108a861ac5a
Francisco Javier Alcala Guzman <francisco.j.alcala.guz@gmail.com>
2025-04-04 01:54:06 +0000

    se agregaron los puertos para poder desplegar la aplicacion

--- Backend/app.py
+++ Backend/app.py
@@ -54,5 +54,5 @@ def handle_file():
     return jsonify(stats)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
